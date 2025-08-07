a979e4f032a037733d8de40980da145d6329aea2
Caleb Skinner <calebskinner01@gmail.com>
2025-08-06 23:16:58 -0500

    trade machine

--- Shiny/Script Support.R
+++ Shiny/Script Support.R
@@ -722,3 +722,26 @@ highest_player_total <- function(value_added, wins_df, enter_round, enter_season
     shiny_edit_tables()
 }
 
+# Trade Machine -----------------------------------------------------------
+grade_trade_wrapper <- function(assets_df, team1_players, team2_players){
+  team1_assets_lost <- assets_df %>% filter(name %in% team2_players)
+  team2_assets_lost <- assets_df %>% filter(name %in% team1_players)
+  
+  team1_assets_gained <- team2_assets_lost %>% mutate(display_name = team1_assets_lost$display_name[1])
+  team2_assets_gained <- team1_assets_lost %>% mutate(display_name = team2_assets_lost$display_name[1])
+  
+  overall_value <- sum(team1_assets_gained$future_value) - sum(team2_assets_gained$future_value)
+  ov_favor <- if_else(overall_value > 0, team1_assets_gained$display_name[1], team2_assets_gained$display_name[1])
+  
+  ov_statement <- str_c("The trade favors ", ov_favor, " by ", round(abs(overall_value), digits = 2), " in total future value")
+  
+  immediate_value <- sum(team1_assets_gained$upcoming_year) - sum(team2_assets_gained$upcoming_year)
+  iv_favor <- if_else(immediate_value > 0, team1_assets_gained$display_name[1], team2_assets_gained$display_name[1])
+  
+  iv_statement <- str_c("The trade favors ", iv_favor, " by ", round(abs(immediate_value), digits = 2), " for the current season")
+  
+  list("team1" = janitor::adorn_totals(team1_assets_gained),
+       "team2" = janitor::adorn_totals(team2_assets_gained),
+       "ov_statement" = ov_statement,
+       "iv_statement" = iv_statement)
+}
