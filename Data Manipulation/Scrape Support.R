f95950e9d8bf05a1940ec815eb8c43def772afb1
Caleb Skinner <calebskinner01@gmail.com>
2025-10-14 09:07:17 -0500

    fix Bam Knight

--- Data Manipulation/Scrape Support.R
+++ Data Manipulation/Scrape Support.R
@@ -87,6 +87,7 @@ name_correction <- function(df){
         name == "Harold Fannin Jr." ~ "Harold Fannin",
         name == "Andres Borregales" ~ "Andy Borregales",
         name == "Tre' Harris" ~ "Tre Harris",
+        name == "Zonovan Knight" ~ "Bam Harris", # bruh
         .default = name))}
 
 # Sleeper API -------------------------------------------------------------
