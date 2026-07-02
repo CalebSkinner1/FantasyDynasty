e3eb0e7adcdfc5899799077d0ccb1883f85affd7
Caleb Skinner <calebskinner01@gmail.com>
2026-07-02 17:05:18 -0500

    update 7/2

--- Workflow/Frequent Updates.R
+++ Workflow/Frequent Updates.R
@@ -37,7 +37,7 @@ source(here::here("Scripts", "History.R"))
 # Relocate Files to be accessed by website
 source(here::here("Scripts", "Relocate Files.R"))
 
-rstudioapi::restartSession() #clear memory
+rstudioapi::restartSession() # clear memory
 
 if (interactive()) {
   setwd(here::here("Shiny"))
